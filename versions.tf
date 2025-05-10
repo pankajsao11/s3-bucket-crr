68ed5ae250f6f682f06897a0f13050261a2bdda7
pankajsao11 <pankajsao11@gmail.com>
2025-05-10 23:24:42 +0530

    created s3 crr files with github action

---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.primary_region
+  alias  = "primary"
+}
+
+provider "aws" {
+  region = var.secondary_region
+  alias  = "secondary"
+}
